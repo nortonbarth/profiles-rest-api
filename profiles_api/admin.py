325d495fafbe2fa99e5cf875eab45898da0a19cb
Norton <nortonbarth@gmail.com>
2023-07-31 10:32:58 -0300

    Finish admin setup

--- profiles_api/admin.py
+++ profiles_api/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
-
+from profiles_api import models
 # Register your models here.
+
+class UserProfile_adminClass(admin.ModelAdmin):
+    list_display = ("email", "is_staff", "is_superuser")
+
+admin.site.register(models.UserProfile,UserProfile_adminClass)
--- profiles_api/models.py
+++ profiles_api/models.py
@@ -3,34 +3,28 @@ from django.contrib.auth.models import AbstractBaseUser
 from django.contrib.auth.models import PermissionsMixin
 from django.contrib.auth.models import BaseUserManager
 
-
-class UserProfileManager(BaseUserManager):
-    """Manager for user profiles"""
-
-    def create_user(self, email, name, password=None):
-        """Create a new user profile"""
-        if not email:
-            raise ValueError("User must have an email address")
-
-        email = self.normalize_email(email)
-        user = self.model(email=email,name=name)
-        user.set_password(password)
-
-        user.save(using=self._db)
-
-        return user
     
 class UserProfileManager(BaseUserManager):
-    """Manager for user profiles"""
 
     def create_user(self, email, name, password=None):
+
+        if email is None:
+            raise TypeError('Users must have an email address.')
+
+        user = self.model(email=self.normalize_email(email),name=name)
+        user.set_password(password)
+        user.save()
+
+        return user
+
+    def create_superuser(self, email, name, password):
         """Create and save new superUser profile"""
+
         
         user =self.create_user(email,name,password)
         user.is_superuser = True
-        user.is_staff = False
+        user.is_staff = True
         user.save(using=self._db)
-
         return user
 
 # Create your models here.
@@ -56,4 +50,4 @@ class UserProfile(AbstractBaseUser, PermissionsMixin):
         return self.name
     
     def __str__(self) -> str:
-        return f'{self.email}'
+        return f'{self.email} '
