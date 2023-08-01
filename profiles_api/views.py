1cce2e65ee5fd55f4838ea6679f87ec27c9cdd83
Norton <nortonbarth@gmail.com>
2023-08-01 13:57:29 -0300

    Added POST, PUT, PATCH and DELEETE methods

--- profiles_api/views.py
+++ profiles_api/views.py
@@ -1,10 +1,13 @@
 from django.shortcuts import render
 from rest_framework.views import APIView
 from rest_framework.response import Response
-
+from rest_framework import status
+from . import serializers
 
 class HelloApiView(APIView):
     """Test API view"""
+
+    serializers_class = serializers.HelloSerializer
 
     def get(self, request, format=None):
         """Return a list of API features"""
@@ -20,5 +23,29 @@ class HelloApiView(APIView):
             'an_apiview':an_apiview
             }
                         )
+
+    def post(self,request):
+        """Create a hello message with our name"""
+        serializer = self.serializers_class(data=request.data)
+
+        if serializer.is_valid():
+            name = serializer.validated_data.get("name")
+            message = f'Hello {name}'
+            return Response({'message':message})
+        else:
+            return Response(serializer.errors,
+                            status=status.HTTP_400_BAD_REQUEST)
+        
+    def put(self,request, pk=None):
+        """Handle and update an object"""
+        return Response({'method':"PUT"})
+        
+    def patch(self, request, pk=None):
+        """Handle a partial update of an object"""
+        return Response({'method':"PATCH"})
+
+    def delete(self, request, pk=None):
+        """Delete an object"""
+        return Response({'method':"DELETE"})
 
 # Create your views here.
--- profiles_project/settings.py
+++ profiles_project/settings.py
@@ -129,3 +129,10 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 
 AUTH_USER_MODEL = 'profiles_api.UserProfile'
+
+REST_FRAMEWORK = {
+    'DEFAULT_RENDERER_CLASSES': [
+        'rest_framework.renderers.JSONRenderer',
+        'rest_framework.renderers.BrowsableAPIRenderer',
+    ]
+}
